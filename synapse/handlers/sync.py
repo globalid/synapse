d55b9dfb58e3b5d03288a64372fba5c610e7519e
Darjan <darjan@global.id>
2024-01-12 15:57:13 +0100

    fix /sync for application services

--- synapse/handlers/sync.py
+++ synapse/handlers/sync.py
@@ -1677,11 +1677,11 @@ class SyncHandler:
         """
 
         user_id = sync_config.user.to_string()
-        app_service = self.store.get_app_service_by_user_id(user_id)
-        if app_service:
-            # We no longer support AS users using /sync directly.
-            # See https://github.com/matrix-org/matrix-doc/issues/1144
-            raise NotImplementedError()
+        # app_service = self.store.get_app_service_by_user_id(user_id)
+        # if app_service:
+        #     # We no longer support AS users using /sync directly.
+        #     # See https://github.com/matrix-org/matrix-doc/issues/1144
+        #     raise NotImplementedError()
 
         sync_result_builder = await self.get_sync_result_builder(
             sync_config,
