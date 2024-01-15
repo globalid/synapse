920ba0f29a911ee4ba6980b12d8b3886f2ef4ca2
Darjan <darjan@global.id>
2024-01-15 13:25:25 +0100

    try to fix /sync for as users

--- synapse/handlers/room.py
+++ synapse/handlers/room.py
@@ -1709,36 +1709,30 @@ class RoomEventSource(EventSource[RoomStreamToken, EventBase]):
             logger.warning("Stream has topological part!!!! %r", from_key)
             from_key = RoomStreamToken(stream=from_key.stream)
 
-        app_service = self.store.get_app_service_by_user_id(user.to_string())
-        if app_service:
-            # We no longer support AS users using /sync directly.
-            # See https://github.com/matrix-org/matrix-doc/issues/1144
-            raise NotImplementedError()
-        else:
-            room_events = await self.store.get_membership_changes_for_user(
+        room_events = await self.store.get_membership_changes_for_user(
                 user.to_string(), from_key, to_key
             )
 
-            room_to_events = await self.store.get_room_events_stream_for_rooms(
-                room_ids=room_ids,
-                from_key=from_key,
-                to_key=to_key,
-                limit=limit or 10,
-                order="ASC",
-            )
+        room_to_events = await self.store.get_room_events_stream_for_rooms(
+                    room_ids=room_ids,
+                    from_key=from_key,
+                    to_key=to_key,
+                    limit=limit or 10,
+                    order="ASC",
+                )
 
-            events = list(room_events)
-            events.extend(e for evs, _ in room_to_events.values() for e in evs)
+        events = list(room_events)
+        events.extend(e for evs, _ in room_to_events.values() for e in evs)
 
-            events.sort(key=lambda e: e.internal_metadata.order)
+        events.sort(key=lambda e: e.internal_metadata.order)
 
-            if limit:
-                events[:] = events[:limit]
+        if limit:
+            events[:] = events[:limit]
 
-            if events:
-                end_key = events[-1].internal_metadata.after
-            else:
-                end_key = to_key
+        if events:
+            end_key = events[-1].internal_metadata.after
+        else:
+            end_key = to_key
 
         return events, end_key
 
